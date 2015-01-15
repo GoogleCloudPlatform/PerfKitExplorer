26b7b1b50cfa85e58df16fe6d24347fb4e887123
Joe Allan Muharsky <jmuharsky@gmail.com>
2015-01-14 17:29:55 -0800

    Add functionality to correctly test admin status on config updates.

--- server/perfkit/explorer/model/explorer_config_test.py
+++ server/perfkit/explorer/model/explorer_config_test.py
@@ -5,6 +5,17 @@ from google.appengine.ext import testbed
 from perfkit.explorer.model import explorer_config
 
 
+ADMIN_USER = {
+  'USER_EMAIL': 'admin@example.com',
+  'USER_ID': '123',
+  'USER_IS_ADMIN': '1'}
+
+NORMAL_USER = {
+  'USER_EMAIL': 'user@example.com',
+  'USER_ID': '456',
+  'USER_IS_ADMIN': '0'}
+
+
 class ExplorerConfigModelTest(unittest.TestCase):
 
   def setUp(self):
@@ -13,11 +24,22 @@ class ExplorerConfigModelTest(unittest.TestCase):
 
     self.testbed.init_datastore_v3_stub()
     self.testbed.init_memcache_stub()
+    self.testbed.init_user_stub()
 
   def tearDown(self):
     self.testbed.deactivate()
 
+  def setAdminStatus(self, is_admin):
+    if is_admin:
+      user_env = ADMIN_USER
+    else:
+      user_env = NORMAL_USER
+
+    self.testbed.setup_env(overwrite = True, **user_env)
+
   def testGetDefault(self):
+    self.setAdminStatus(is_admin=False)
+
     expected_config = {
         'default_project': explorer_config.DEFAULT_PROJECT,
         'default_dataset': explorer_config.DEFAULT_DATASET,
@@ -30,7 +52,19 @@ class ExplorerConfigModelTest(unittest.TestCase):
 
     self.assertEquals(expected_config, actual_config)
 
+  def testUpdateRejectForNonAdmin(self):
+    self.setAdminStatus(is_admin=False)
+
+    provided_data = {'default_project': 'MODIFIED_PROJECT'}
+
+    self.assertRaises(
+      explorer_config.SecurityError,
+      explorer_config.ExplorerConfigModel.Update,
+      provided_data)
+
   def testUpdateDefault(self):
+    self.setAdminStatus(is_admin=True)
+
     provided_project = 'MODIFIED_PROJECT'
     provided_data = {'default_project': provided_project}
 
@@ -48,6 +82,8 @@ class ExplorerConfigModelTest(unittest.TestCase):
     self.assertEquals(expected_config, actual_config)
 
   def testUpdateExisting(self):
+    self.setAdminStatus(is_admin=True)
+
     provided_project = 'MODIFIED_PROJECT'
     provided_data = {'default_project': provided_project}
 
@@ -70,6 +106,7 @@ class ExplorerConfigModelTest(unittest.TestCase):
     self.assertEquals(expected_config, actual_config)
 
   def testLoad(self):
+    self.setAdminStatus(is_admin=True)
     provided_project = 'MODIFIED_PROJECT'
     provided_data = {'default_project': provided_project}
 
