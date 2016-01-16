6a20e3140c4de3a010cf4bc23a48b5f1e11af525
Klaus Weidner <klausw@google.com>
2016-01-15 17:31:37 -0800

    Enable jscompiler warnings.
    This is still a work in progress, 216 warnings left down from >600.
    
    Updates lib/closure-library to current 'master' to fix warnings
    in the library.
    
    Adds client/externs/ files for third-party APIs from:
    
      https://github.com/google/closure-compiler/tree/master/contrib/externs/
      http://www.google.com/uds/modules/gviz/gviz-api.js

--- client/externs.js
+++ client/externs.js
@@ -22,16 +22,27 @@
  * @author joemu@google.com (Joe Allan Muharsky)
  */
 
-angular.$httpBackend = function() {};
-angular.$httpBackend.prototype.whenGET = function(a) {};
-angular.$httpBackend.prototype.whenPOST = function(a) {};
-angular.$httpBackend.prototype.passThrough = function() {};
-angular.$httpBackend.prototype.respond = function(a) {};
+var jasmine = {};
+jasmine.createSpy = function() {};
 
+function CodeMirror() {};
 CodeMirror.prototype.setValue = function(a) {};
 
 function history() {}
 history.pushState = function(a, b, c) {};
 
-var CURRENT_USER_ADMIN = null;
-var CURRENT_USER_EMAIL = null;
+// TODO(klausw): not sure why these aren't being picked up automatically.
+// Prevents "Bad type annotation" error for IArrayLike in goog/object/object.js
+
+/**
+ * @interface
+ * @template KEY1, VALUE1
+ */
+function IObject() {}
+
+/**
+ * @interface
+ * @extends {IObject<number, VALUE2>}
+ * @template VALUE2
+ */
+function IArrayLike() {}
