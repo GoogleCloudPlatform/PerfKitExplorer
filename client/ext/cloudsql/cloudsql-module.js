ff8e65da314513440644bd200637c669fa33a063
Joe Allan Muharsky <jmuharsky@gmail.com>
2016-03-18 14:54:29 -0700

    Add datastore and UX for setting cloudsql admin config.

--- client/ext/cloudsql/cloudsql-module.js
+++ client/ext/cloudsql/cloudsql-module.js
@@ -1,6 +1,7 @@
 goog.provide('p3rf.perfkit.explorer.ext.cloudsql.module');
 
-goog.require('p3rf.perfkit.explorer.ext.cloudsql.CloudsqlDatasourceModel');
+goog.require('p3rf.perfkit.explorer.ext.cloudsql.CloudsqlConfigService');
+goog.require('p3rf.perfkit.explorer.ext.cloudsql.CloudsqlConfigDirective');
 goog.require('p3rf.perfkit.explorer.ext.cloudsql.CloudsqlDatasourceDirective');
 
 
@@ -9,5 +10,8 @@ goog.scope(function() {
   
   cloudsql.module = angular.module('pkx.cloudsql', []);
 
+  cloudsql.module.service('cloudsqlConfigService', cloudsql.CloudsqlConfigService);
+
+  cloudsql.module.directive('cloudsqlConfig', cloudsql.CloudsqlConfigDirective);
   cloudsql.module.directive('cloudsqlDatasource', cloudsql.CloudsqlDatasourceDirective);
 });
